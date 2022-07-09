77e1a8b864b3ad3118322f59b46b48466ae09426
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-07-09 15:11:04 +0200

    ♻️ refactor streaming logic

--- stream_logic.py
+++ stream_logic.py
@@ -1,25 +1,52 @@
 import base64
+import sys
 
 import cv2
-import numpy as np
 import zmq
 
 from constants import *
 
-context = zmq.Context()
-footage_socket = context.socket(zmq.SUB)
-footage_socket.bind(f"tcp://*:{PORT}")
-footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(""))
 
-while True:
+def connect_to_viewer(server_address, port):
+    context = zmq.Context()
+    footage_socket = context.socket(zmq.PUB)
+    footage_socket.connect(f"tcp://{server_address}:{port}")
+    return footage_socket
+
+
+def start_streaming(footage_socket, camera_mode=0):
+    print("Streaming Started...")
+    camera = cv2.VideoCapture(camera_mode)  # init the camera
+    while True:
+        try:
+            grabbed, frame = camera.read()  # grab the current frame
+            encoded, buffer = cv2.imencode('.jpg', frame)
+            jpg_as_text = base64.b64encode(buffer)
+            footage_socket.send(jpg_as_text)
+
+        except KeyboardInterrupt:
+            camera.release()
+            cv2.destroyAllWindows()
+            break
+
+
+def main():
+    port = PORT
+    server_address = SERVER_ADDRESS
+
     try:
-        frame = footage_socket.recv_string()
-        img = base64.b64decode(frame)
-        npimg = np.fromstring(img, dtype=np.uint8)
-        source = cv2.imdecode(npimg, 1)
-        cv2.imshow("Stream", source)
-        cv2.waitKey(1)
+        if len(sys.argv) > 1:
+            program_name = sys.argv[0]
+            arguments = sys.argv[1:]
+            count = len(arguments)
+            server_address = arguments[0]
+            port = arguments[1]
+    except IndexError as ie:
+        print("Loading default Server Address and Port.")
 
-    except KeyboardInterrupt:
-        cv2.destroyAllWindows()
-        break
+    sock = connect_to_viewer(server_address=server_address, port=port)
+    start_streaming(sock)
+
+
+if __name__ == '__main__':
+    main()
